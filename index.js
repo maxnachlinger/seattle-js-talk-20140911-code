4792509238cede7e6150aa672ded159663160959
Max Nachlinger <max.nachlinger@gmail.com>
2014-09-07 21:17:32 -0700

    Naming

--- index.js
+++ index.js
@@ -57,12 +57,12 @@ server.pack.register([
 		plugin: require('good')
 	},
 	{
-		plugin: require('../../'),
+		plugin: require('hapi-l10n-gettext'),
 		options: {
 			cookieName: '_locale',
 			l10nDirectory: path.resolve(__dirname, 'locales'),
 			defaultLocale: 'en',
-			excludeRoutes: [assetRoute, localeSetRoute],
+			excludedRoutes: [assetRoute, localeSetRoute],
 			includedRoutes: routes
 		}
 	}
